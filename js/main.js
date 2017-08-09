02ec5338ba54051160ba6c42ee5ff48108fcb41b
cglsoft <cglsoft@hotmail.com>
2017-08-09 01:29:04 -0300

    Implementação do campo de pesquisa com filtro em objetos HTML

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-const CLIENT_ID = 'qjcy28dvrj8o2hp969uhnqs46m2lb3'
+const CLIENT_ID = 'qjcy28dvrj8o2hp969uhnqs46m2lb3';
 const ROOT_URL = 'https://api.twitch.tv/kraken/';
 const USER_TWITCHTV = ["cglsoft","ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
-//const USER_TWITCHTV = ["ESL_SC2"];
 
 var aUserInfo = [];
 
@@ -32,7 +31,7 @@ drawHtmlDiv = (jsonTwitch) =>{
   let idxAchou = aUserInfo.indexOf(jsonTwitch.name);
   let statusLin = ( idxAchou === -1) ? 'offline' : 'online';
 
-  html =  '<div id="' + statusLin + '" class="card  text-center">' +
+  html =  '<div id="' + statusLin + '" class="card text-center ' + jsonTwitch.display_name + '">' +
   '    <img class="img-fluid rounded-circle w-25 p-1" src="'+ jsonTwitch['logo'] + '" alt="Logo TwitchTV">' +
   '    <div class="card-block"> '+
   '      <h4 class="card-title"> '+ jsonTwitch.display_name + '</h4>' +
@@ -41,7 +40,6 @@ drawHtmlDiv = (jsonTwitch) =>{
   '      <small class="text-muted"> <a href="' + jsonTwitch.url +'" class="card-link">'+jsonTwitch.status+'<a> </small>' +
   '      </div> ' +
   '  </div>';
-  // console.log( 'User: ' + jsonTwitch.name + ' Array: ' + aUserInfo +  '- IndexOF:' +  aUserInfo.indexOf(jsonTwitch.name) );
   cardElement.insertAdjacentHTML('afterbegin',html);
 }
 
@@ -51,27 +49,50 @@ twitchTVHTMLView = () => USER_TWITCHTV.forEach( (user) =>   getInfo( "channels",
 // Carga do status do usuário ONLINE ou OFFLINE
 // Esta rotina precisa ser executada primeiro, pois irá impactar na segunda função.
 let wait = (ms) => new Promise(resolve => {
+  aUserInfo = [];
   USER_TWITCHTV.forEach( (user) =>  getInfo("streams",user) );
   setTimeout(resolve, ms)
 });
 
 // Balancear a carga dos eventos
 let loadPageInfo = async () => {
+  removeElementoHtml();
+
   await wait(2000);
   await this.twitchTVHTMLView();
 }
 
+// Remover elementos para não duplicar!
+removeElementoHtml = () => {
+  let elems = document.querySelectorAll("#online, #offline");
+
+  Array.prototype.forEach.call(elems, (item) => item.remove());
+}
+
 // Filtro para remover o objeto
-filtroOnline = (filtro) => {
+filtroHtml = (filtro) => {
   let elems    = document.querySelectorAll(filtro);
   let elemsAll = document.querySelectorAll("#online, #offline");
 
-    // Mostrar todos // elems[i].remove();  -- Remove o objeto definitivo!
-  Array.prototype.forEach.call(elemsAll, (item) => item.style.display = 'block');
-  // Filtro
+  // Mostrar todos // elems[i].remove();  -- Remove o objeto definitivo!
+  Array.prototype.forEach.call(elemsAll, (item) => item.style.display = 'inline-block');
+  // Filtro esconder os elementos
   Array.prototype.forEach.call(elems, (item) => item.style.display = 'none');
 }
 
+// Pesquisar elemento
+SearchHml = () => {
+  let elems = document.querySelectorAll("#online, #offline");
+
+  let textElem = '.' + document.getElementById("inputPesq").value;
+  let elem = document.querySelectorAll(textElem);
+
+  // Filtro esconder os elementos
+  Array.prototype.forEach.call(elems, (item) => item.style.display = 'none');
+
+  // Mostrar somente o search text
+  Array.prototype.forEach.call(elem, (item) => item.style.display = 'inline-block');
+}
 
 // Carregar página principal
 loadPageInfo();
